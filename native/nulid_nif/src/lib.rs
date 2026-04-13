016de5d036ed4a9c4e47bbacc0c0816305d89e4e
kakilangit <kakilangit@gmail.com>
2026-04-13 09:33:34 +0200

    enforce clippy pedantic, forbid unsafe/unwrap/expect/panic in NIF crate

--- native/nulid_nif/src/lib.rs
+++ native/nulid_nif/src/lib.rs
@@ -1,3 +1,7 @@
+#![warn(clippy::pedantic)]
+#![forbid(unsafe_code, clippy::unwrap_used, clippy::expect_used, clippy::panic)]
+#![allow(clippy::needless_pass_by_value)]
+
 use nulid::{DefaultGenerator, DistributedGenerator, Nulid};
 use rustler::{Binary, Env, NewBinary, NifResult, Resource, ResourceArc};
 
@@ -11,7 +15,7 @@ impl Resource for GeneratorResource {}
 impl Resource for DistributedGeneratorResource {}
 
 fn nulid_from_binary(binary: Binary) -> NifResult<Nulid> {
-    Nulid::try_from(binary.as_slice()).map_err(nulid_err_to_rustler)
+    Nulid::try_from(binary.as_slice()).map_err(|e| nulid_err_to_rustler(&e))
 }
 
 fn nulid_to_binary<'a>(env: Env<'a>, nulid: &Nulid) -> Binary<'a> {
@@ -21,7 +25,7 @@ fn nulid_to_binary<'a>(env: Env<'a>, nulid: &Nulid) -> Binary<'a> {
     bin.into()
 }
 
-fn nulid_err_to_rustler(err: nulid::Error) -> rustler::Error {
+fn nulid_err_to_rustler(err: &nulid::Error) -> rustler::Error {
     rustler::Error::Term(Box::new(format!("{err}")))
 }
 
@@ -29,12 +33,12 @@ fn nulid_err_to_rustler(err: nulid::Error) -> rustler::Error {
 fn new() -> NifResult<String> {
     Nulid::new()
         .map(|id| id.to_string())
-        .map_err(nulid_err_to_rustler)
+        .map_err(|e| nulid_err_to_rustler(&e))
 }
 
 #[rustler::nif]
-fn new_binary<'a>(env: Env<'a>) -> NifResult<Binary<'a>> {
-    let id = Nulid::new().map_err(nulid_err_to_rustler)?;
+fn new_binary(env: Env<'_>) -> NifResult<Binary<'_>> {
+    let id = Nulid::new().map_err(|e| nulid_err_to_rustler(&e))?;
     Ok(nulid_to_binary(env, &id))
 }
 
@@ -46,20 +50,24 @@ fn encode(binary: Binary) -> NifResult<String> {
 
 #[rustler::nif]
 fn decode<'a>(env: Env<'a>, string: &str) -> NifResult<Binary<'a>> {
-    let nulid: Nulid = string.parse().map_err(nulid_err_to_rustler)?;
+    let nulid: Nulid = string.parse().map_err(|e| nulid_err_to_rustler(&e))?;
     Ok(nulid_to_binary(env, &nulid))
 }
 
 #[rustler::nif]
 fn nanos(binary: Binary) -> NifResult<u64> {
     let nulid = nulid_from_binary(binary)?;
-    Ok(nulid.nanos() as u64)
+    u64::try_from(nulid.nanos()).map_err(|e| nulid_err_to_rustler_string(&e))
 }
 
 #[rustler::nif]
 fn millis(binary: Binary) -> NifResult<u64> {
     let nulid = nulid_from_binary(binary)?;
-    Ok(nulid.millis() as u64)
+    u64::try_from(nulid.millis()).map_err(|e| nulid_err_to_rustler_string(&e))
+}
+
+fn nulid_err_to_rustler_string(err: &impl std::fmt::Display) -> rustler::Error {
+    rustler::Error::Term(Box::new(format!("{err}")))
 }
 
 #[rustler::nif]
@@ -85,15 +93,18 @@ fn generator_generate(resource: ResourceArc<GeneratorResource>) -> NifResult<Str
         .0
         .generate()
         .map(|id| id.to_string())
-        .map_err(nulid_err_to_rustler)
+        .map_err(|e| nulid_err_to_rustler(&e))
 }
 
 #[rustler::nif]
-fn generator_generate_binary<'a>(
-    env: Env<'a>,
+fn generator_generate_binary(
+    env: Env<'_>,
     resource: ResourceArc<GeneratorResource>,
-) -> NifResult<Binary<'a>> {
-    let id = resource.0.generate().map_err(nulid_err_to_rustler)?;
+) -> NifResult<Binary<'_>> {
+    let id = resource
+        .0
+        .generate()
+        .map_err(|e| nulid_err_to_rustler(&e))?;
     Ok(nulid_to_binary(env, &id))
 }
 
@@ -112,15 +123,18 @@ fn distributed_generator_generate(
         .0
         .generate()
         .map(|id| id.to_string())
-        .map_err(nulid_err_to_rustler)
+        .map_err(|e| nulid_err_to_rustler(&e))
 }
 
 #[rustler::nif]
-fn distributed_generator_generate_binary<'a>(
-    env: Env<'a>,
+fn distributed_generator_generate_binary(
+    env: Env<'_>,
     resource: ResourceArc<DistributedGeneratorResource>,
-) -> NifResult<Binary<'a>> {
-    let id = resource.0.generate().map_err(nulid_err_to_rustler)?;
+) -> NifResult<Binary<'_>> {
+    let id = resource
+        .0
+        .generate()
+        .map_err(|e| nulid_err_to_rustler(&e))?;
     Ok(nulid_to_binary(env, &id))
 }
 
